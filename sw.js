c047461d7aa375b88d4deccb3547ae80d8d8265e
Llyod-17 <parker4real17@gmail.com>
2026-05-29 13:24:52 +0700

    akhir

--- sw.js
+++ sw.js
@@ -1,7 +1,29 @@
+const CACHE = "signal-terakhir-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "https://fonts.googleapis.com/css2?family=Share+Tech+Mono&family=VT323&display=swap",
+];
+
 self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS).catch(() => {})));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)),
+        ),
+      ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
